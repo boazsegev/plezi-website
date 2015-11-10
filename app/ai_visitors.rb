ca40840117665909c239354e209001c60aeff6f2
Boaz Segev <Boaz@2be.co.il>
2015-11-10 02:24:03 -0500

    non-websocket AI

--- app/ai_visitors.rb
+++ app/ai_visitors.rb
@@ -5,36 +5,62 @@ class AIConnection
 		"Hi!",
 		"hi :-)",
 		"hi",
+		'Hey',
+		'hey',
+		'hello',
+		'Hello',
+		'hola',
+		'Hola',
 		"yo!",
-		"YO :-)",
 		"Yo",
-		"I think I saw someone with my handle! Not fair!",
+		"bye",
+		"Bye",
 		"hello world!",
 		"hi world",
 		"hello world",
 		"test",
 		"cool",
 		"nice",
+		"nice :)",
+		"lol",
+		"LOL",
 		"works",
+		"works!",
+		"hi ya'll",
+		"YO :-)",
+		"eat THIS, node.js :p",
+		"I think I saw someone with my handle! Not fair!",
 		"Anybody there?",
 		"I'm GOD!",
 		"I love Ruby",
-		"hi ya'll",
-		"lol",
 		"Help! I think I'm an AI...!",
-		"bye",
-		"Bye"
+		"I spy guides",
+		"I spy source code",
+		"I know you're there!",
+		"I know you can read this"
 		]
+	MESSAGES[0..24].each {|m| MESSAGES << m}
 	def initialize
-		pause = rand(8..16)/5.0
-		name = NAMES.sample
-		message = MESSAGES.sample
-		options = {}
-		options[:on_open] = Proc.new { Iodine.run_after(pause) { write message; Iodine.run_after(pause) { close } } } 
-		options[:on_message] = Proc.new {|data| }
-		options[:url] = "ws://localhost:#{Iodine.port}/#{name}"
-		Iodine::Http.ws_connect options
+		@name = NAMES.sample
+		HomeController.broadcast :print, "#{@name} joind the chat."
+		Iodine.run_after(pause) { post_message }
+		# options = {}
+		# options[:on_open] = Proc.new { Iodine.run_after(pause) { write MESSAGES.sample; rand(1..7).even? ? (Iodine.run_after(pause) { close }) : Iodine.run_after(pause) {on_open} } }
+		# options[:on_message] = Proc.new {|data| }
+		# options[:url] = "ws://localhost:#{Iodine.port}/#{name}"
+		# Iodine::Http.ws_connect options
+	end
+	def post_message
+		HomeController.broadcast :print, "#{@name}: #{MESSAGES.sample}"
+		return Iodine.run_after(pause) { post_message } unless rand(1..7).even? # last?
+		Iodine.run_after(pause) { leave }
+	end
+	def pause
+		rand(8..24)/4.0
+	end
+	def leave
+		HomeController.broadcast :print, "#{@name} left the chat."
 	end
 end
 
-Iodine.run_every(16) { AIConnection.new }
+Iodine.run_every(6) { AIConnection.new }

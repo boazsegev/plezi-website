5099d4fc3f22bc0353961945ef1a6b7fcc5e216b
Boaz Segev <Boaz@2be.co.il>
2015-11-15 05:07:22 -0500

    outbound links to open in new window

--- app/docs.rb
+++ app/docs.rb
@@ -1,5 +1,5 @@
 # The basic website controller, to get you started
-class GuidesController
+class DocsController
 	# HTTP
 	def index
 		# return response << "Hello World!" # for a hello world app
@@ -10,13 +10,23 @@ class GuidesController
 	end
 end
 
+
+
+class NewPageLinksMDRenderer < Redcarpet::Render::HTML
+	# Other methods where we don't return only a specific argument
+	def link(link, title, content)
+		"<a href=\"#{link}\" #{"target='_blank' " unless link[0] =~ /[\/\.]/}title=\"#{title}\">#{content}</a>"
+	end
+end
+
+
 # # register the Github makrdown flavor renderer
 # ::Plezi::Renderer.register :md do |filename, context, &block|
 # 	Plezi.cache_needs_update?(filename) ? Plezi.cache_data( filename, ::GitHub::Markup.render(filename) )  : (Plezi.get_cached filename)
 # end
 
-MD_RENDERER = Redcarpet::Markdown.new Redcarpet::Render::HTML.new( with_toc_data: true), autolink: true, fenced_code_blocks: true, no_intra_emphasis: true, tables: true, footnotes: true
-# MD_RENDERER = Redcarpet::Markdown.new Redcarpet::Render::HTML, with_toc_data: true, autolink: true, fenced_code_blocks: true
+MD_RENDERER = Redcarpet::Markdown.new NewPageLinksMDRenderer.new(with_toc_data: true), autolink: true, fenced_code_blocks: true, no_intra_emphasis: true, tables: true, footnotes: true
+# MD_RENDERER = Redcarpet::Markdown.new Redcarpet::Render::HTML.new( with_toc_data: true), autolink: true, fenced_code_blocks: true, no_intra_emphasis: true, tables: true, footnotes: true
 MD_RENDERER_TOC = Redcarpet::Markdown.new Redcarpet::Render::HTML_TOC.new()
 # register the Makrdown renderer with some Github flavors (but not the official Github Renderer)
 ::Plezi::Renderer.register :md do |filename, context, &block|
